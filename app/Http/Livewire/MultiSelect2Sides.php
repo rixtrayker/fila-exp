c7d544459888d45aaa03dd1c99f62e9f903515a9
Amr M. Elsayed <rixtrayker@github.com>
2023-04-06 00:47:57 +0200

    Disable clicks on view plan

--- app/Http/Livewire/MultiSelect2Sides.php
+++ app/Http/Livewire/MultiSelect2Sides.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Livewire;
 
+use App\Filament\Resources\PlanResource\Pages\ViewPlan;
+use Closure;
+use Str;
 use LucasGiovanny\FilamentMultiselectTwoSides\Forms\Components\Fields\MultiselectTwoSides;
 
 class MultiSelect2Sides extends MultiselectTwoSides
 {
     protected string $view = 'livewire.multi-select2-sides';
+
+    public function defaultSelectOptions(array |Closure | null $options): static {
+        $this->afterStateHydrated(fn($component)=>$component->state($options));
+        return $this;
+    }
+
+    public function selectOption(string $value): void
+    {
+        if(Str::contains(request()->fingerprint['name'],'view-plan')) return;
+        $state = array_unique(array_merge($this->getState(), [$value]));
+        $this->state($state);
+    }
+
+    public function unselectOption(string $value): void
+    {
+        if(Str::contains(request()->fingerprint['name'],'view-plan')) return;
+        $state = $this->getState();
+        unset($state[array_search($value, $state)]);
+        $this->state($state);
+    }
+
+    public function selectAll(): void
+    {
+        if(Str::contains(request()->fingerprint['name'],'view-plan')) return;
+        $this->state(array_keys($this->getOptions()));
+    }
+
+    public function unselectAll(): void
+    {
+        if(Str::contains(request()->fingerprint['name'],'view-plan')) return;
+        $this->state([]);
+    }
 }
