e4acb35ae38702abd29a7b8cc0ff8ff66bd1ae11
Amr M. Elsayed <rixtrayker@github.com>
2023-04-07 03:11:14 +0200

    Add nested tree of manager_id

--- app/Models/Scopes/GetMineScope.php
+++ app/Models/Scopes/GetMineScope.php
@@ -5,6 +5,7 @@ namespace App\Models\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\DB;
 
 class GetMineScope implements Scope
 {
@@ -20,5 +21,25 @@ class GetMineScope implements Scope
         if(auth()->user() && auth()->user()->hasRole('medical-rep')){
             $builder->where('user_id', '=', auth()->id());
         }
+
+
+
+        if(auth()->user() && auth()->user()->hasRole(['super-admin','country-manager','area-manager','district-manager'])) {
+            $query = DB::table('users')
+            ->selectRaw('id')
+                ->where('manager_id',auth()->id())
+                ->unionAll(
+                    DB::table('users')
+                        ->select('users.id')
+                        ->join('tree', 'tree.id', '=', 'users.manager_id')
+                );
+
+            $tree = DB::table('tree')
+                ->withRecursiveExpression('tree', $query)
+                ->pluck('id')->toArray();
+
+            $builder->whereIn('user_id', $tree);
+        }
+
     }
 }
