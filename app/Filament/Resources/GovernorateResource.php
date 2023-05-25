2f70d1c4cfee0b234837b22b7cf48fbbf2e2fe7b
Amr M. Elsayed <rixtrayker@github.com>
2023-05-25 15:31:00 +0300

    re sort

--- app/Filament/Resources/GovernorateResource.php
+++ app/Filament/Resources/GovernorateResource.php
@@ -27,7 +27,7 @@ class GovernorateResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     protected static ?string $navigationGroup = 'Zone management';
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 3;
 
     public static function form(Form $form): Form
     {
