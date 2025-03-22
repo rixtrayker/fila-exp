8729421ec1504fd12b98b48d4045d7c7d68b6274
Amr M. Elsayed <cs.elsayed@gmail.coms>
2025-03-22 02:20:47 +0200

    Refactor settings table migration to adjust column order and improve schema clarity. Ensure 'key' column is positioned correctly and maintain nullable attributes for 'type' and 'description'.

--- database/migrations/2025_03_22_020051_alter_settings_table.php
+++ database/migrations/2025_03_22_020051_alter_settings_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
         DB::table('settings')->truncate();
 
         Schema::table('settings', function (Blueprint $table) {
-            $table->integer('order')->default(0);
-            $table->string('name')->nullable();
-            $table->string('key')->unique()->change();
-            $table->string('type')->nullable();
-            $table->string('description')->nullable();
+            $table->integer('order')->default(0)->after('id');
+            $table->string('key')->unique()->after('order');
+            $table->string('type')->nullable()->before('name');
+            $table->string('description')->nullable()->after('name');
         });
     }
 
@@ -29,7 +28,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('settings', function (Blueprint $table) {
-            $table->string('key')->unique()->change();
+            $table->dropColumn('key');
             $table->dropColumn('order');
             $table->dropColumn('name');
             $table->dropColumn('description');
