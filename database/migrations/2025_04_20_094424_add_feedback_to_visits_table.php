b747a5adedfdd1833cc65ce4fc89642298f167da
Amr M. Elsayed <cs.elsayed@gmail.coms>
2025-04-20 11:56:12 +0200

    fix(migration): adjust feedback column placement in visits table migration
    - Updated the migration for the visits table to position the 'feedback' column after the 'comment' column, ensuring a more logical order in the database schema.
    - This change enhances the clarity of the data structure and improves the organization of fields within the visits table.

--- database/migrations/2025_04_20_094424_add_feedback_to_visits_table.php
+++ database/migrations/2025_04_20_094424_add_feedback_to_visits_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('visits', function (Blueprint $table) {
-            $table->string('feedback')->nullable();
+            $table->string('feedback')->after('comment')->nullable();
         });
     }
 
