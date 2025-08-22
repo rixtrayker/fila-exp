c0417bab035a58279fbbb199d57967f6ee419976
Amr M. Elsayed <cs.elsayed@gmail.coms>
2025-08-22 22:23:19 +0300

    refactor(VisitCompletionChart): optimize visit data query for monthly statistics
    This commit updates the VisitCompletionChart widget to utilize Carbon for determining the start and end dates of the current month. The query is modified to fetch visit data based on these dates, enhancing the accuracy of the monthly visit completion statistics presented in the chart.

--- app/Filament/Resources/VisitResource/Widgets/VisitCompletionChart.php
+++ app/Filament/Resources/VisitResource/Widgets/VisitCompletionChart.php
@@ -5,8 +5,8 @@ namespace App\Filament\Resources\VisitResource\Widgets;
 use App\Models\Visit;
 use App\Helpers\DateHelper;
 use Filament\Widgets\ChartWidget;
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Carbon\Carbon;
 
 class VisitCompletionChart extends ChartWidget
 {
@@ -17,10 +17,11 @@ class VisitCompletionChart extends ChartWidget
     {
         $today = DateHelper::today();
         $mineUsers = User::getMine()->pluck('id');
+        $startOfMonth = Carbon::today()->startOfMonth()->format('Y-m-d');
+        $endOfMonth = Carbon::today()->endOfMonth()->format('Y-m-d');
 
-        // Single query to get all counts at once
         $visitStats = Visit::whereIn('user_id', $mineUsers)
-            ->whereBetween('visit_date', DateHelper::currentMonth()['start'], DateHelper::currentMonth()['end'])
+            ->whereBetween('visit_date', [$startOfMonth, $endOfMonth])
             ->whereNotNull('plan_id')
             ->selectRaw('
                 COUNT(*) as total_planned_visits,
